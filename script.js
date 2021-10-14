6fe3bb29bd46b19ff4f69c49679c4d38ef99c842
Taylor W <taylorwilliams@pursuit.org>
2021-10-14 12:26:16 -0400

    Updated JavaScript

--- script.js
+++ script.js
@@ -5,16 +5,42 @@ fetch('https://ghibliapi.herokuapp.com/films')
     let dropDown = document.querySelector('#dropdown');
     for (let film of films) {
         const option = document.createElement('option')
-        document.querySelector('option');
         option.setAttribute('value', film.title)
         option.textContent = film.title
         dropDown.append(option);
+
+        
+    
+        dropDown.addEventListener('change', () => {
+            for (let film of films) {
+                if (dropDown.value === film.title) {
+                    let display = document.querySelector('#display-info');
+                    let name = film.title;
+                    let year = film.release_date;
+                    let description = film.description;
+                    display.innerHTML = 
+                    `<p><strong>${name}</strong></p><p>${year}</p><p>${description}</p>`
+                }
+            }
+        })
+
+
     }
+
     
     
+    let form = document.querySelector('form')
+    .addEventListener('submit', (event) => {
+        event.preventDefault()
+        const userInput = event.target.review.value;
+        const ul = document.querySelector('ul')
+        const li = document.createElement('li')
+        li.textContent = userInput;
+        ul.append(li);
 
 
-
-
-})
+        document.querySelector('form').reset();
+        }
+     )
+    })
 
